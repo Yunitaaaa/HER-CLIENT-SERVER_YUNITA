4ab96f9c7d692b837b40af9d93f9272d385c5013
Yunitaaaa <113493671+Yunitaaaa@users.noreply.github.com>
2023-01-25 10:16:34 -0800

    Add files via upload
    JDK 17
    Packaging : Jar
    Spring Boot : 2.7.5

---
+++ anggota-service/src/main/java/com/Yunita/anggota/service/repository/AnggotaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.anggota.service.repository;
+
+import com.Yunita.anggota.service.entity.Anggota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author acer
+ */
+@Repository
+public interface AnggotaRepository extends JpaRepository<Anggota, Long> {
+
+    public Anggota findByAnggotaId(Long anggotaId);
+    
+}
